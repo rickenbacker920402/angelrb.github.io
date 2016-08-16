cf250dcb50ccee91613e9fe6e5d681f51d54ef03
angelrb <fs_254@hotmail.com>
2016-08-16 15:34:44 -0500

    Create ejemplo1.js

---
+++ rv/ejemplo1.js
@@ -0,0 +1,14 @@
+var miCanvas1 = document.getElementById("miCanvas1");
+var contexto1 = miCanvas1.getContext("2d");
+contexto.fillStyle = "red";
+contexto.fillRect(30,30,50,50);
+
+var miCanvas2 = document.getElementById("miCanvas2");
+var contexto2 = miCanvas2.getContext("2d");
+contexto.fillStyle = "green";
+contexto.fillRect(90,30,50,50);
+
+var miCanvas3 = document.getElementById("miCanvas3");
+var contexto3 = miCanvas3.getContext("2d");
+contexto.fillStyle = "blue";
+contexto.fillRect(150,30,50,50);
