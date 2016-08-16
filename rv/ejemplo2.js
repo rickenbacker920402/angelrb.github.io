2fb96686763b57dca390e90e7ee3bf555bc9532c
angelrb <fs_254@hotmail.com>
2016-08-16 15:49:58 -0500

    Update ejemplo2.js

--- rv/ejemplo2.js
+++ rv/ejemplo2.js
@@ -3,5 +3,5 @@
 var miCanvas2 = document.getElementById("miCanvas2");
 var contexto2 = miCanvas2.getContext("2d");
 contexto.fillStyle = "green";
-contexto.fillRect(90,30,50,50);
+contexto.fillRect(20,30,40,50);
 
