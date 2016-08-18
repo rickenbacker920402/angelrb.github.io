b3dda6a44618272ff9e26d6d355f29d753221e6a
angelrb <fs_254@hotmail.com>
2016-08-17 23:16:56 -0500

    Update green.js

--- rv/green.js
+++ rv/green.js
@@ -3,5 +3,5 @@
 var miCanvas2 = document.getElementById("miCanvas2");
 var contexto2 = miCanvas2.getContext("2d");
 contexto2.fillStyle = "green";
-contexto2.fillRect(90,90,120,120);
+contexto2.fillRect(90,90,100,100);
 
