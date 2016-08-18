262c5bef032110affa7cc6353301ae3de1c2d456
angelrb <fs_254@hotmail.com>
2016-08-17 22:23:45 -0500

    Update red.js

--- rv/red.js
+++ rv/red.js
@@ -1,5 +1,5 @@
 var miCanvas1 = document.getElementById("miCanvas1");
 var contexto1 = miCanvas1.getContext("2d");
-contexto.fillStyle = "red";
-contexto.fillRect(10,20,30,40);
+contexto1.fillStyle = "red";
+contexto1.fillRect(10,20,30,40);
 
