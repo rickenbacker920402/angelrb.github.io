f19940bc1d15f330846f7ee41dbf020a11ebc966
angelrb <fs_254@hotmail.com>
2016-08-18 15:50:15 -0500

    Update ejemplo-4.js

--- rv/ejemplo-4.js
+++ rv/ejemplo-4.js
@@ -1,7 +1,7 @@
 var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
-var renderizador = new THREE.WebGLRednderer();
+var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.donElement);
 var forma=new THREE.BoxGeometry(1,1,1);
