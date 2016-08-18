f1a9123f1dbed11051e24931343b0a0c69f8a764
angelrb <fs_254@hotmail.com>
2016-08-17 22:24:21 -0500

    Update and rename ejemplo3.js to blue.js

--- rv/blue.js
+++ rv/blue.js
@@ -2,5 +2,5 @@
 
 var miCanvas3 = document.getElementById("miCanvas3");
 var contexto3 = miCanvas3.getContext("2d");
-contexto.fillStyle = "blue";
-contexto.fillRect(150,30,50,50);
+contexto3.fillStyle = "blue";
+contexto3.fillRect(150,30,50,50);
