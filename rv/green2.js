a26194ed8da7098481b19e41344704941d5212cd
angelrb <fs_254@hotmail.com>
2016-08-17 22:23:25 -0500

    Update and rename ejemplo2.js to green2.js

--- rv/green2.js
+++ rv/green2.js
@@ -2,6 +2,6 @@
 
 var miCanvas2 = document.getElementById("miCanvas2");
 var contexto2 = miCanvas2.getContext("2d");
-contexto.fillStyle = "green";
-contexto.fillRect(20,30,40,50);
+contexto2.fillStyle = "green";
+contexto2.fillRect(20,30,40,50);
 
