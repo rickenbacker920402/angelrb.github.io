27e7ecce15e1eb6fde1a8304d8aa252af60dbdd7
angelrb <fs_254@hotmail.com>
2016-08-25 15:19:08 -0500

    Update blue.js

--- rv/blue.js
+++ rv/blue.js
@@ -3,4 +3,4 @@
 var miCanvas3 = document.getElementById("miCanvas3");
 var contexto3 = miCanvas3.getContext("2d");
 contexto3.fillStyle = "blue";
-contexto3.fillRect(60,60,100,100);
+contexto3.fillRect(60,60,10,10);
