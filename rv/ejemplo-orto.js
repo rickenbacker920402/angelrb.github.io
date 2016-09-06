4728349e2c10c77960a7ad2a24434b96de922a08
angelrb <fs_254@hotmail.com>
2016-09-06 15:33:42 -0500

    Update and rename ejemplo-ortografica.js to ejemplo-orto.js

--- rv/ejemplo-orto.js
+++ rv/ejemplo-orto.js
@@ -9,16 +9,13 @@ camara.updateProjectionMatrix();
 
 camara.position.z = 100;
 
-var cubo = new THREE.Mesh( new THREE.BoxGeometry(30, 30, 30 ),
-                            new THREE.MeshNormalMaterial() );
+var cubo = new THREE.Mesh( new THREE.BoxGeometry(30, 30, 30 ), new THREE.MeshNormalMaterial() );
 cubo.rotateY(Math.PI/4);
 
-var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 15 ),
-                              new THREE.MeshNormalMaterial() );
+var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMaterial() );
 esfera1.position.x = 50;
 
-var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 15 ),
-                              new THREE.MeshNormalMaterlal() );
+var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMaterlal() );
 
 esfera2.position.x = -50;
 esfera2.position.z = -100;
