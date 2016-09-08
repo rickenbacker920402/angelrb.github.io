6a53199508e54d8e563241cc08f3d62e644242f0
angelrb <fs_254@hotmail.com>
2016-09-08 15:15:37 -0500

    Update cubo1.js

--- rv/cubo1.js
+++ rv/cubo1.js
@@ -1,5 +1,13 @@
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var cube = new THREE.Mesh( geometry, material );
+var malla = new THREE.Mesh( geometry, material );
+malla.rotateX(Math.PI/4);
 var escena = new THREE.Scene();
-escena.add( cube );
+escena.add( malla );
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,
+                    window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
