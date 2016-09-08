0305747fa19f39c0d545655695b2442c15153dbc
angelrb <fs_254@hotmail.com>
2016-09-08 14:57:47 -0500

    Update cubo1.js

--- rv/cubo1.js
+++ rv/cubo1.js
@@ -1,4 +1,4 @@
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
