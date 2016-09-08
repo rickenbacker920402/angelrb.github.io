2bd52882771100c00d69707e2d7a9ee2cf87975d
angelrb <fs_254@hotmail.com>
2016-09-08 15:39:34 -0500

    Update cubo1.js

--- rv/cubo1.js
+++ rv/cubo1.js
@@ -1,4 +1,4 @@
-var geometry = new THREE.BoxGeometry( 10, 10, 10 );
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshNormalMaterial(  );
 var malla = new THREE.Mesh( geometry, material );
 malla.rotateX(Math.PI/4);
