e298949aa202a7f7ff2cd395cdb9007c5de75e8b
angelrb <fs_254@hotmail.com>
2016-09-08 15:37:14 -0500

    Update cubo1.js

--- rv/cubo1.js
+++ rv/cubo1.js
@@ -14,4 +14,5 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,
                     window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
+
 renderizador.render(escena,camara);
