e3e8f0a62857b0bf121b21baa121ee5e40170c8e
angelrb <fs_254@hotmail.com>
2016-09-08 16:06:58 -0500

    Update cubo1.js

--- rv/cubo1.js
+++ rv/cubo1.js
@@ -4,6 +4,8 @@ var malla = new THREE.Mesh( geometry, material );
 malla.rotateX(Math.PI/4);
 malla.rotateY(Math.PI/4);
 
+var tablero = [];
+
 
 var escena = new THREE.Scene();
 escena.add( malla );
@@ -17,3 +19,5 @@ renderizador.setSize(window.innerHeight*.95,
 document.body.appendChild(renderizador.domElement);
 
 renderizador.render(escena,camara);
+
+https://github.com/mrdoob/three.js/blob/master/examples/canvas_camera_orthographic.html
