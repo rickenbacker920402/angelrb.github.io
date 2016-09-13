b5c2a061e0bf4bdd05a8f63efc6fec36587c5015
angelrb <fs_254@hotmail.com>
2016-09-13 15:24:50 -0500

    Update ejemplo-iluminacion.js

--- rv/ejemplo-iluminacion.js
+++ rv/ejemplo-iluminacion.js
@@ -11,7 +11,7 @@ escena.add(iluminacion);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
-var lienzo = document.getElementById("luzAmbiental");
+var lienzo = document.getElementById("AmbientLight");
 var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
 
 renderizador.setSize(600,600);
