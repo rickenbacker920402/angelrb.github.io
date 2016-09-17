c9a5c933d0a6865fe2db929cc206d2d01779a989
angelrb <fs_254@hotmail.com>
2016-09-16 21:46:10 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -11,9 +11,9 @@ camara.lookAt(new THREE.Vector3(45,45,0));
 
 //ILUMINACION
 var iluminacion=new THREE.PointLight(0xFFFFFF);
-iluminacion.position.x=-70;
-iluminacion.position.y=-70;
-iluminacion.position.z=0;
+iluminacion.position.x=-30;
+iluminacion.position.y=-30;
+iluminacion.position.z=-30;
 
 
 //CASILLA BLANCA
