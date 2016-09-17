4aa44459bae0a3685106b8355620edf4b47d0027
angelrb <fs_254@hotmail.com>
2016-09-16 21:19:39 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -65,10 +65,7 @@ var escena = new THREE.Scene();
 for(var i=1; i<=32; i++){
 casillaBlanca[i] = new THREE.Mesh(formaCasillaBlanca, mallaCasillaBlanca);
 casillaGris[i] = new THREE.Mesh(formaCasillaGris, mallaCasillaGris);
-casillaBlanca[i].castShadow = true;
-casillaBlanca[i].receiveShadow = true;
-casillaGris[i].castShadow = true;
-casillaGris[i].receiveShadow = true;
+
   }
 
 for(var f=1; f<=8; f++)
@@ -117,7 +114,10 @@ for(var f=1; f<=8; f++)
   }
 }
 
-
+casillaBlanca[].castShadow = true;
+casillaBlanca[].receiveShadow = true;
+casillaGris[].castShadow = true;
+casillaGris[].receiveShadow = true;
 
 escena.add(Tablero);
 escena.add(torreMalla);
