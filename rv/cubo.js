1d4e563b9cf1b086e5fbb47763dee911b1d8712f
angelrb <fs_254@hotmail.com>
2016-09-16 22:08:48 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -1,38 +1,44 @@
-var forma = new THREE.Geometry();
-forma.vertices.push(new THREE.Vector3( 0, 0, 0));
-forma.vertices.push(new THREE.Vector3( 1, 0, 0));
-forma.vertices.push(new THREE.Vector3( 0, 0, 1));
-forma.vertices.push(new THREE.Vector3( 0, 1, 0));
-forma.vertices.push(new THREE.Vector3( 1, 1, 0));
-forma.vertices.push(new THREE.Vector3( 0, 1, 1));
-forma.vertices.push(new THREE.Vector3( 1, 1, 1));
+var forma=new THREE.Geometry();
 
-forma.faces.push(new THREE.Face3( 5, 4, 0));
-forma.faces.push(new THREE.Face3( 0, 1, 5));
-forma.faces.push(new THREE.Face3( 0, 1, 2));
-forma.faces.push(new THREE.Face3( 1, 3, 2));
-forma.faces.push(new THREE.Face3( 5, 7, 1));
-forma.faces.push(new THREE.Face3( 7, 3, 1));
-forma.faces.push(new THREE.Face3( 0, 4, 6));
-forma.faces.push(new THREE.Face3( 0, 6, 2));
-forma.faces.push(new THREE.Face3( 2, 6, 7));
-forma.faces.push(new THREE.Face3( 2, 7, 3));
+forma.vertices.push(new THREE.Vector3(-1,1,1),
+                    new THREE.Vector3(-1,1,-1),
+                    new THREE.Vector3(1,1,-1),
+                    new THREE.Vector3(1,1,1),
+                    new THREE.Vector3(-1,-1,1),
+                    new THREE.Vector3(-1,-1,-1),
+                    new THREE.Vector3(1,-1,-1),
+                    new THREE.Vector3(1,-1,1));
 
-forma.computeBoundingSphere();
+forma.faces.push(new THREE.Face3(0,1,2),
+                new THREE.Face3(0,2,3),
+                new THREE.Face3(0,3,4),
+                new THREE.Face3(7,4,3),
+                new THREE.Face3(7,4,5),
+                new THREE.Face3(6,7,5),
+                new THREE.Face3(2,5,6),
+                new THREE.Face3(2,1,5),
+                new THREE.Face3(1,0,5),
+                new THREE.Face3(4,5,0),
+                new THREE.Face3(6,2,7),
+                new THREE.Face3(3,7,2));
+
+forma.computeBoundingSphere(); //¿¿¿Para qué funciona ésta esfera???
 
 forma.computeFaceNormals();
 
 var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh(forma,material);
+malla.rotateX(-Math.PI/2);
+malla.rotateY(Math.PI/4);
+malla.rotateZ(-Math.PI/3);
 
-var malla = new THREE.Mesh( forma, material );
-malla.rotateX(Math.PI/4);
 
 var escena = new THREE.Scene();
-escena.add( malla );
+escena.add(malla);
+
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95,
-                    window.innerHeight*.95);
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
